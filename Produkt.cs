d63c358d2fe2211a909a532cffda256ce27ec984
FridaBergkvist2 <fridaber10@student.astar.se>
2025-06-30 15:54:56 +0200

    Uppdaterar Program och Produkt med info

---
+++ Produkt.cs
@@ -0,0 +1,16 @@
+public class Produkt
+{
+    public string Namn { get; set; }
+    public double Pris { get; set; }
+
+    public Produkt(string namn, double pris)
+    {
+        Namn = namn;
+        Pris = pris;
+    }
+
+    public void VisaInfo()
+    {
+        Console.WriteLine($"{Namn} - {Pris} kr");
+    }
+}
