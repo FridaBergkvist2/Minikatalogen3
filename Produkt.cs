1a72e93a2c6e04363cfd8d860804791c70b4b29d
FridaBergkvist2 <fridaber10@student.astar.se>
2025-07-04 11:16:52 +0200

    Lagt till Produkt och Program

--- Produkt.cs
+++ Produkt.cs
@@ -1,7 +1,28 @@
 public class Produkt
 {
-    public string Namn { get; set; }
-    public double Pris { get; set; }
+private string namn;
+    private double pris;
+
+public string Namn;
+{
+    get { return; name; }
+    set 
+{ 
+if (!string.IsNullOrWhiteSpace(value))
+namn = value; 
+}
+    }
+
+public double Pris
+    {
+        get { return pris; }
+        set
+        {
+            if (value >= 0)
+                pris = value;
+        }
+
+    }
 
     public Produkt(string namn, double pris)
     {
@@ -9,7 +30,7 @@ public class Produkt
         Pris = pris;
     }
 
-    public void VisaInfo()
+    public virtual void VisaInfo()
     {
         Console.WriteLine($"{Namn} - {Pris} kr");
     }
--- Program.cs
+++ Program.cs
@@ -6,66 +6,13 @@ class Program
     static void Main()
     {
         List<Produkt> produkter = new List<Produkt>();
-        bool kör = true;
 
-        while (kör)
-        {
-Console.Clear();
-Console.WriteLine("Fridas Minikatalog 2");
-Console.WriteLine("1. Lägg till produkt");
-Console.WriteLine("2. Visa alla produkter");
-Console.WriteLine("3. Avsluta");
-Console.Write("Välj ett alternativ (1-3):");
+        produkter.Add(new Produkt("USB-laddare", 99));
+        produkter.Add(new Bok("C# på riktigt", 249, "Anna Svensson"));
 
-string val = Console.ReadLine();
-
-switch (val)
-            {
-case "1":     
-{   
-Console.Write("Ange produktnamn (eller skriv 'stopp' för att avsluta): ");
-            string namn = Console.ReadLine();
-
-            if (namn.ToLower() == "stopp")
-            {
-                kör = false;
-                break;
-            }
-
-            Console.Write("Ange pris: ");
-            double pris = Convert.ToDouble(Console.ReadLine());
-
-            produkter.Add(new Produkt(namn, pris));
-
-        Console.WriteLine("\n Produkten har lagts till");
-Console.WriteLine("Tryck på valfri tangent för att fortsätta.");
-                    Console.ReadKey();
-                    break;
-}
-case "2":
-{ 
-Console.WriteLine("\n Alla produkter i Fridas katalog");
-if (produkter.Count == 0)
-        {
-            Console.WriteLine("Lägg till en produkt först för tusan.");
-        }
-else
-        {
         foreach (Produkt p in produkter)
         {
-p.VisaInfo();
-    }
-}
-                    Console.WriteLine("Tryck på valfri tangent för att fortsätta.");
-                    Console.ReadKey();
-                    break;
-                }
-case "3":
-{ 
-kör = false;
-break;
-}
-            }
+            p.VisaInfo();
         }
     }
 }
